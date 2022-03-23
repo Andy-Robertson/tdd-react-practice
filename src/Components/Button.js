3a13db1dbdde229be39fbc8cb07eead8deea6483
AndyR <50073313+Andy-Robertson@users.noreply.github.com>
2022-03-23 15:04:54 +0000

    Add random tests and refactor code

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -3,7 +3,13 @@ import React, { useState } from "react";
 const Button = () => {
   const [count, setCount] = useState(0);
 
-  const countClickHandler = () => setCount((count) => count + 1);
+  const countClickHandler = () => {
+    if (count === 3) {
+      setCount(0);
+    } else {
+      setCount((count) => count + 1);
+    }
+  };
 
   return (
     <button data-testid="button" onClick={countClickHandler}>
--- src/tests/Button.test.js
+++ src/tests/Button.test.js
@@ -1,14 +1,18 @@
 import Button from "../Components/Button";
-
 import { render, screen, fireEvent } from "@testing-library/react";
-// import userEvent from "@testing-library/user-event";
 
 // Using TDD, write a react component with a button that displays a number, starting with 0.
-
 // The number should increase by 1 every time you click the button.
 // Once that works, modify your component so that after 3 it cycles back to 0 again
 
 describe("Button component", () => {
+  const simulateButtonClick = (timesToRun) => {
+    while (timesToRun-- > 0 && timesToRun <= 3) {
+      fireEvent.click(screen.getByTestId("button"));
+    }
+    return;
+  };
+
   it("renders on page", () => {
     render(<Button />);
 
@@ -27,20 +31,60 @@ describe("Button component", () => {
 
   it("displays a count equal to 1 after the button has been clicked once", () => {
     render(<Button />);
+
     const buttonId = screen.getByTestId("button");
 
-    fireEvent.click(buttonId);
+    simulateButtonClick(1);
 
     expect(Number(buttonId.innerHTML)).toBe(1);
   });
 
-    it("displays a count equal to 2 after the button has been clicked twice", () => {
-      render(<Button />);
-      const buttonId = screen.getByTestId("button");
+  it("displays a count equal to 2 after the button has been clicked twice", () => {
+    render(<Button />);
+    const buttonId = screen.getByTestId("button");
 
-      fireEvent.click(buttonId);
-      fireEvent.click(buttonId);
+    simulateButtonClick(2);
 
-      expect(Number(buttonId.innerHTML)).toBe(2);
-    });
+    expect(Number(buttonId.innerHTML)).toBe(2);
+  });
+
+  it("displays a count equal to 3 after the button has been clicked three times", () => {
+    render(<Button />);
+    const buttonId = screen.getByTestId("button");
+
+    simulateButtonClick(3);
+
+    expect(Number(buttonId.innerHTML)).toBe(3);
+  });
+
+  it("displays a count equal to 0 after the button is clicked four or more times", () => {
+    render(<Button />);
+    const buttonId = screen.getByTestId("button");
+
+    simulateButtonClick(4);
+
+    expect(Number(buttonId.innerHTML)).toBe(0);
+  });
+
+  it("displays a count equal to 0 after the button is clicked a random number of times greater than 3 but less then 100", () => {
+    render(<Button />);
+    const buttonId = screen.getByTestId("button");
+
+    const randomTimesToRun = parseInt(Math.random() * (99 - 4) + 4);
+
+    simulateButtonClick(randomTimesToRun);
+
+    expect(Number(buttonId.innerHTML)).toBe(0);
+  });
+
+  it("displays a count equal to a random number after the button is clicked equal to 3 or less", () => {
+    render(<Button />);
+    const buttonId = screen.getByTestId("button");
+
+    const randomTimesToRun = parseInt(Math.random() * 3);
+
+    simulateButtonClick(randomTimesToRun);
+
+    expect(Number(buttonId.innerHTML)).toBe(randomTimesToRun);
+  });
 });
