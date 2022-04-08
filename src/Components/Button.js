0cb4aaf1db17f9a6224de2f4e38acd952a72e94b
AndyR <50073313+Andy-Robertson@users.noreply.github.com>
2022-04-08 13:16:30 +0100

    Convert functional button component to a class component

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,24 +1,46 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
-const Button = () => {
-  const [count, setCount] = useState(0);
+// const Button = () => {
+//   const [count, setCount] = useState(0);
 
-  const countClickHandler = () => {
-    setCount((count) => {
-      return (count + 1) % 4;
-      // if (count === 3) {
-      //   return 0;
-      // } else {
-      //   return count + 1;
-      // }
+//   const countClickHandler = () => {
+//     setCount((count) => {
+//       return (count + 1) % 4;
+//       // if (count === 3) {
+//       //   return 0;
+//       // } else {
+//       //   return count + 1;
+//       // }
+//     });
+//   };
+
+//   return (
+//     <button data-testid="button" onClick={countClickHandler}>
+//       {count}
+//     </button>
+//   );
+// };
+
+// export default Button;
+
+import React, { Component } from "react";
+
+export default class Button extends Component {
+  state = { count: 0 };
+
+  increment = () => {
+    this.setState((state) => {
+      return {
+        count: (state.count + 1) % 4,
+      };
     });
   };
 
-  return (
-    <button data-testid="button" onClick={countClickHandler}>
-      {count}
-    </button>
-  );
-};
-
-export default Button;
+  render() {
+    return (
+      <button data-testid="button" onClick={this.increment}>
+        {this.state.count}
+      </button>
+    );
+  }
+}
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,13 +1,28 @@
 import React from "react";
 
+const onSubmitHandler = (e) => {
+  e.preventDefault();
+  console.log("Submitted Form");
+};
+
 const Form = () => {
   return (
-    <form name="form">
-      <label htmlFor="name" typeof="text">Name:</label>
+    <form name="form" onSubmit={onSubmitHandler}>
+      <label htmlFor="name" typeof="text">
+        Name:
+      </label>
       <input id="name" />
-      <label htmlFor="email" typeof="text">Email:</label>
+      <label htmlFor="email" typeof="text">
+        Email:
+      </label>
       <input id="email" />
-      <button>Submit</button>
+      <button
+        type="submit"
+        name="submit"
+        onClick={() => console.log("Clicked Submit")}
+      >
+        Submit
+      </button>
     </form>
   );
 };
